8a9bf08463132f35130a8f8e19b8049de9fbd39c
geoffrey-saunois <geoffrey@stockly.ai>
2025-12-02 14:36:47 +0100

    Day 2

--- day2.py
+++ day2.py
@@ -27,14 +27,50 @@ def first_problem(id_ranges: list[list[int]]) -> int:
     for id_range in id_ranges:
         curr = first_invalid_half(id_range[0])
         while double_number(curr) <= id_range[1]:
-            # print(f"Adding double number {double_number(curr)}")
             ans += double_number(curr)
             curr += 1
     return ans
 
 
-def second_problem(id_ranges):
-    return 0
+def second_problem(id_ranges, verbose=False) -> int:
+    ans = 0
+    for a, b in id_ranges:
+        ans += sum_multiple_numbers(a, b, verbose=verbose)
+    return ans
+
+
+def sum_multiple_numbers(start_range: int, end_range: int, verbose=False) -> int:
+    count = 0
+    for n in range(start_range, end_range + 1):
+        if is_multiple_number(n):
+            count += n
+            if verbose:
+                print(f"Found multiple number for range {start_range}-{end_range}: {n}")
+    return count
+
+
+def is_multiple_number(n: int) -> bool:
+    mask = 10
+    while mask <= n:
+        if is_nth_number(n, mask):
+            return True
+        mask *= 10
+    return False
+
+
+def is_nth_number(n: int, mask: int) -> bool:
+    pattern = n % mask
+    n = n // mask
+
+    # Make sure the first digit  is not 0
+    if pattern < mask // 10:
+        return False
+
+    while n > 0:
+        if n % mask != pattern:
+            return False
+        n = n // mask
+    return True
 
 
 def parse_input(path):
@@ -46,14 +82,16 @@ def parse_input(path):
 
 
 if __name__ == "__main__":
+    print(f"{day} answers:")
 
     # print("test input:", parse_input(test_path))
     # print("val input:", parse_input(val_path))
 
-    lines = parse_input(test_path)
-    print("First problem:", first_problem(lines))
-    print("Second problem:", second_problem(lines))
+    test = parse_input(test_path)
+    val = parse_input(val_path)
 
-    lines = parse_input(val_path)
-    print("First problem:", first_problem(lines))
-    print("Second problem:", second_problem(lines))
+    print("First problem test :", first_problem(test))
+    print("First problem val:", first_problem(val))
+
+    print("Second problem test:", second_problem(test, verbose=True))
+    print("Second problem val:", second_problem(val))
